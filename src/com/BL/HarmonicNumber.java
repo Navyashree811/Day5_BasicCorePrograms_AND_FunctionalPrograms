d12c4aa458442b8991f8874a303c12d939725e60
hp-pc <navyashree811@gmail.com>
2022-08-04 13:31:49 +0530

    [Navaya] Added : Refactring the Name of file

--- src/com/BL/HarmonicNumber.java
+++ src/com/BL/HarmonicNumber.java
@@ -20,7 +20,7 @@ public class HarmonicNumber {
 	public static void main(String[] args) {
 		System.out.println("Enter the number to check the harmonic number : ");
 		int number = HarmonicNumber.getIntValue();
-		HarmonicNumber.getHarmonicNumber(number);
+		 HarmonicNumber.getHarmonicNumber(number);
 	}
 
 }
